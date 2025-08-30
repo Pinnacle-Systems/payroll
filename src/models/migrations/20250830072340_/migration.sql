f07572d4dee6a5907fe5c926bc56a81d19220f5d
karthick <karthickcs10124@gmail.com>
2025-08-30 16:30:46 +0530

    karthick

--- src/models/migrations/20250830072340_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `payfrequencyitems` ADD COLUMN `payFrequencyId` INTEGER NULL;
-
--- AddForeignKey
-ALTER TABLE `PayFrequencyItems` ADD CONSTRAINT `PayFrequencyItems_payFrequencyId_fkey` FOREIGN KEY (`payFrequencyId`) REFERENCES `PayFrequency`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
