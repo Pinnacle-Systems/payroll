c2bef5688a885b6e8036bd4b5e6808a8e51b794f
Manoj <manojbharathi00@gmail.com>
2025-09-02 18:53:18 +0530

    12

--- src/models/sampleQuery.js
+++ src/models/sampleQuery.js
@@ -1,24 +1,9 @@
-// import { PrismaClient } from "@prisma/client";
-// const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-// let poInwardReturnItemsCount = await prisma.poInwardReturnItems.count({
-//     where: {
-//         poItemsId: 75
-//     }
-// })
-// console.log(poInwardReturnItemsCount)
-
-// prismaClient.js
-const { PrismaClient } = require('@prisma/client');
-
-const globalForPrisma = global;
-
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['error', 'warn'], // Optional: logs queries & warnings
-  });
-
-globalForPrisma.prisma = prisma;
-
-module.exports = { prisma };
+let poInwardReturnItemsCount = await prisma.poInwardReturnItems.count({
+  where: {
+    poItemsId: 75
+  }
+})
+console.log(poInwardReturnItemsCount)
